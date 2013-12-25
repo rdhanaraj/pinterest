e6de2230be512dad94750f530cc08c1328cd49b3
Rishi Dhanaraj <rishid97@gmail.com>
2013-12-24 16:10:19 -0800

    Added user functionality with Devise gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Pinterest::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # For Devise gem
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Pinterest::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Required for Heroku
+  # Note to update with actual host name
+  config.action_mailer.default_url_options = { :host => 'thenewpinterest.herokuapp.com' }
 end
